b21f600dc5b513155841f83f6b3ca2bff3ad074d
Carry Wang <design.carrywang@gmail.com>
2023-09-22 11:15:35 +0800

    add lession3

---
+++ lesson3/StorageFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageList;
+
+    function createSimpleStorageContract() public {
+        simpleStorageList.push(new SimpleStorage());
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        simpleStorageList[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageList[_simpleStorageIndex].retrieve();
+    }
+}
